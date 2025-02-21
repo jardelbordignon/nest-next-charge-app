1ffd3e54614bba90a123a55fa6cbe26e82d05f33
Jardel Bordignon <jardel1101@gmail.com>
2025-02-21 00:04:46 -0300

    ✨ fastify configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,20 @@
 import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
 import { AppModule } from './app.module'
 import { EnvService } from './infra/env/env.service'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  )
+
   const port = app.get(EnvService).get('PORT')
   const logger = new Logger('Bootstrap')
 
   await app
-    .listen(port)
+    .listen(port, '0.0.0.0')
     .then(async () => {
       global.apiHost = await app.getUrl()
       logger.log(`🚀 Server is running on: ${global.apiHost}`)
