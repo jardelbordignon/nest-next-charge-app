4462e4b59583fb0c5e273515ed487d74735c38ef
Jardel Bordignon <jardel1101@gmail.com>
2025-02-21 00:06:32 -0300

    ✨ swagger configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 import { EnvService } from './infra/env/env.service'
 
@@ -11,7 +12,28 @@ async function bootstrap() {
   )
 
   const port = app.get(EnvService).get('PORT')
+  const environment = app.get(EnvService).get('NODE_ENV')
   const logger = new Logger('Bootstrap')
+
+  app.enableCors()
+
+  if (environment !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Node Pay')
+      .setDescription('The NodeJS Pay api documentation')
+      .setVersion('0.1')
+      .addBearerAuth()
+      .build()
+
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('doc', app, document, {
+      swaggerOptions: {
+        apisSorter: 'alpha',
+        operationsSorter: 'alpha',
+        tagsSorter: 'alpha',
+      },
+    })
+  }
 
   await app
     .listen(port, '0.0.0.0')
