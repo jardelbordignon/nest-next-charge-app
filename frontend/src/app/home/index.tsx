f199759faeb13503682805509bbe496a2ca0226b
Jardel Bordignon <jardel1101@gmail.com>
2025-02-24 05:44:43 -0300

    init: frontend

---
+++ frontend/src/app/home/index.tsx
@@ -0,0 +1,7 @@
+export function HomePage() {
+  return (
+    <div className='flex items-center justify-center gap-8 flex-col lg:flex-row h-screen max-w-[calc(100vw - (100vw - 1160px) / 2)'>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
